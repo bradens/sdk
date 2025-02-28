d8549a4bfea508effc4e125e68dd33d12ce03b13
Braden Simpson <bradensimpsons@gmail.com>
2025-02-28 12:37:55 -0700

    chore: update example

--- examples/simple/filterTest.ts
+++ examples/simple/filterTest.ts
@@ -1,12 +1,14 @@
-import { Codex } from '@codex-data/sdk'
+import { Codex } from "@codex-data/sdk";
 
-const sdk = new Codex(process.env.CODEX_API_KEY || "")
+const sdk = new Codex(process.env.CODEX_API_KEY || "");
 
-sdk.queries.filterTokens({
-  filters: {
-    liquidity: { gt: 100000 },
-    txnCount24: { gt: 200 },
-    network: [1],
-  },
-  limit: 10
-}).then(console.log)
+sdk.queries
+  .filterTokens({
+    filters: {
+      liquidity: { gt: 100000 },
+      txnCount24: { gt: 200 },
+      network: [1],
+    },
+    limit: 1,
+  })
+  .then((t) => console.log(JSON.stringify(t, null, 2)));
